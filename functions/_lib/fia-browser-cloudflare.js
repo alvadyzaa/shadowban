2c005404c4b5b396022b3d73c5cc060959caaa1f
Alva <me@alva.web.id>
2026-03-27 00:06:10 +0700

    Load Cloudflare Playwright at runtime

--- functions/_lib/fia-browser-cloudflare.js
+++ functions/_lib/fia-browser-cloudflare.js
@@ -1,7 +1,13 @@
 import { runFiaScanInPage } from './fia-browser-shared.js';
 
+async function loadCloudflarePlaywright() {
+  const dynamicImport = new Function('specifier', 'return import(specifier);');
+  const moduleName = ['@cloudflare', 'playwright'].join('/');
+  return dynamicImport(moduleName);
+}
+
 export async function fetchFiaDeepScanWithCloudflareBrowser(username, browserBinding) {
-  const { launch } = await import('@cloudflare/playwright');
+  const { launch } = await loadCloudflarePlaywright();
   const browser = await launch(browserBinding);
 
   try {
